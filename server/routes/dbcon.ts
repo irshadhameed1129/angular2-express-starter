e458ab47ea8c86ca67ef06bc88c010ed4e18f1c4
premji0133 <30617577+premji0133@users.noreply.github.com>
2017-08-01 13:43:28 +0530

    Create dbcon.ts

---
+++ server/routes/dbcon.ts
@@ -0,0 +1,10 @@
+import { Sequelize } from "sequelize";
+
+
+
+
+var sequelize = new Sequelize('postgres://ilaqzxyrlnxnlz:9650172c6ba4165e53764f34cc160c3de16b19520780774fdec14ab70e54d380@ec2-50-17-217-166.compute-1.amazonaws.com:5432/d514sq7qc4t8g5');
+
+
+export {   Sequelize, sequelize };
+
