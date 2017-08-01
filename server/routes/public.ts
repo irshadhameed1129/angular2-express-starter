5ccb65cfb168e5576b4f576993a89c8d962f70ca
premji0133 <30617577+premji0133@users.noreply.github.com>
2017-08-01 14:00:21 +0530

    Update public.ts

--- server/routes/public.ts
+++ server/routes/public.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-
+import { User } from './usermodel';
 const publicRouter: Router = Router();
 
 publicRouter.get("/simple", (request: Request, response: Response) => {
@@ -8,5 +8,35 @@ publicRouter.get("/simple", (request: Request, response: Response) => {
     title: "Greetings.",
   });
 });
+
+publicRouter.get('/list', (request: Request, response: Response) => {
+ 
+
+// force: true will drop the table if it already exists
+  
+  Users.findAll({
+  attributes: ['firstName', 'lastName']
+}).
+then(users => {
+  response.json({
+  users
+  });
+ 
+});
+});
+
+
+ publicRouter.get('/add_user', (request: Request, response: Response) => {
+ Tas_product.sync({force: true}).then(() => {
+  //Table created
+  return Tas_product.create({
+    NAME: 'Ghee',
+    HAS_STOCK: true,
+    IS_POOJA: false,
+     });
+ });
+   
+ });
+
 
 export { publicRouter };
