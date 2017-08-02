b790722018fe6a51e4c75348e43dfdb4466656f8
premji0133 <30617577+premji0133@users.noreply.github.com>
2017-08-02 15:23:15 +0530

    Update dash.ts

--- src/app/dash/dash.ts
+++ src/app/dash/dash.ts
@@ -1,1 +1,17 @@
-e
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators, FormBuilder }  from '@angular/forms';
+@Component({
+  selector: 'app-dash',
+  templateUrl: './dash.html',
+  styleUrls: ['./dash.css']
+})
+export class DashComponent implements OnInit {
+  constructor( private router: Router) {   }
+  ngOnInit()  {}
+ login() {
+   
+   console.log('insisi');
+   
+ }
+  }
