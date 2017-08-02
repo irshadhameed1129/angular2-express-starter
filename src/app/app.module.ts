063725e9f0e340ec239928578968857886e6565c
premji0133 <30617577+premji0133@users.noreply.github.com>
2017-08-02 15:36:06 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.router';
-import { LoginComponent } from './dash/dash';
-import { DashComponent } from './login/login';
+import { DashComponent } from './dash/dash';
+import { LoginComponent } from './login/login';
 import {ReactiveFormsModule} from "@angular/forms";
 
 
