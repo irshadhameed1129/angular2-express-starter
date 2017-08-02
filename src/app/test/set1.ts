b01b40f84d0339164493348c423d81f642e00523
premji0133 <30617577+premji0133@users.noreply.github.com>
2017-08-02 16:48:32 +0530

    Update set1.ts

--- src/app/test/set1.ts
+++ src/app/test/set1.ts
@@ -11,6 +11,7 @@ import { FormGroup, FormControl, Validators, FormBuilder }  from '@angular/forms
 export class Set1Component implements OnInit {
   
    favoriteSeason: string;
+  qes: number =1;
      
   seasons = [  'രാവിലെ','രാത്രി','കസ്റ്റമര്‍ക്ക് ഇഷ്ടമുള്ളപ്പോള്‍', 'ഉച്ചയ്ക്ക് ശേഷം', ];
   constructor( private router: Router) {   }
@@ -21,4 +22,12 @@ export class Set1Component implements OnInit {
    console.log('insisi');
    
  }
+  next()
+  {  
+    this.qes = this.qes+1;
+  }
+  prev()
+  {  
+    this.qes = this.qes-1;
+  }
   }
