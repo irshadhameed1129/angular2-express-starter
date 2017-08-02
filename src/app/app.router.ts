b3ad694bf2e316fca15dfaf8577e39addee4276d
premji0133 <30617577+premji0133@users.noreply.github.com>
2017-08-02 15:55:23 +0530

    Update app.router.ts

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,13 +2,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
 import { DashComponent } from './dash/dash';
 import { NgModule } from '@angular/core';
-
+import { Set1Component } from './test/set1';
 
 export const routes: Routes = [ 
   { path:'', redirectTo: '/login', pathMatch: 'full' },
     { path: '', component: LoginComponent},
     { path: 'login', component: LoginComponent },
    { path: 'dash', component: DashComponent },
+  {path: 'set1', component: Set1Component }
  
   
   
