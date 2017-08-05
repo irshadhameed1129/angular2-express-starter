aef83fb0c35f9d7312d894b761132df5c437a5b7
premji0133 <30617577+premji0133@users.noreply.github.com>
2017-08-05 19:19:19 +0530

    Update adduser.ts

--- src/app/dash/adduser.ts
+++ src/app/dash/adduser.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from './usermodel';
 import { FormGroup, FormControl, Validators, FormBuilder }  from '@angular/forms';
 @Component({
   selector: 'add-user',
@@ -9,6 +10,10 @@ import { FormGroup, FormControl, Validators, FormBuilder }  from '@angular/forms
 export class AdduserComponent implements OnInit {
   
   myForm: FormGroup;
+   
+    model = new User('','');
+
+        
   
   constructor( private router: Router,private fb: FormBuilder) {   }
   ngOnInit()  {
@@ -20,7 +25,12 @@ export class AdduserComponent implements OnInit {
               }
  login() {
    
-   console.log(this.myForm.value);
+
+   this.model.username = this.myForm.controls.username.value;
+   this.model.password = this.myForm.controls.password.value;
+   let body= JSON.stringify(this.model);
+   console.log(body);
+   console.log(this.myForm.controls.username.value);
    
        }
   }
