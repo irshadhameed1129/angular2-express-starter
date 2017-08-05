b22f8823c062bd4f36fcd2209763baf2cceb61e1
premji0133 <30617577+premji0133@users.noreply.github.com>
2017-08-05 21:44:45 +0530

    Update login.service.ts

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,1 +1,29 @@
-ts
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { contentHeaders } from './headers' ;
+import 'rxjs/add/operator/map'
+
+@Injectable()
+export class AuthenticationService {
+    constructor(private http: Http) { }
+
+    login(model:any) {
+
+        let body= JSON.stringify(model); 
+
+        
+        return this.http.post('https://assespmkvy.herokuapp.com/api/public/add_user', body, { headers: contentHeaders})
+            .map((response: Response) => {
+                
+                
+                {
+                    let postr = response.json();
+                   console.log('responce=',postr)
+                    
+                }
+            })   .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+            
+            
+    }
+}
