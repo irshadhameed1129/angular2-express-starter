7a86771da8d172287fb1504e2e4125ba43b17671
premji0133 <30617577+premji0133@users.noreply.github.com>
2017-08-06 08:10:00 +0530

    Update login.ts

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,16 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators, FormBuilder }  from '@angular/forms';
+import { AuthenticationService } from '../services/login.service';
+import { User } from '../dash/usermodel';
+import {MdDialog} from '@angular/material';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.html',
   styleUrls: ['./login.css']
 })
 export class LoginComponent implements OnInit {
-  constructor( private router: Router) {   }
-  ngOnInit()  {}
- login() {
-   console.log('insisi');
-   this.router.navigate(['/dash']);
- }
+ 
+  myForm: FormGroup;
+  model = new User('','');
+   color = 'primary';
+  mode = 'indeterminate';
+  value = 50;
+  bufferValue = 75;
+  constructor( private router: Router,private fb: FormBuilder,public dialog: MdDialog, private aS: AuthenticationService) {   }
+  ngOnInit()  {
+   this.myForm = this.fb.group({
+            username: [''],
+            password: ['']             
+                                      });
+
   }
+   
+     
+    
+      
+
+
+ login() {
+   
+
+    this.aS.login(this.myForm.value)
+    .subscribe(data => { 
+                    
+                     
+                      this.router.navigate(['/dash']);
+                     
+                },
+                
+                error => {
+                    this.router.navigate(['/']);
+                    
+                });
+   
+        }
+  }
+
+
