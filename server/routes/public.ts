2bad30b80a597034ac79446693b376a9731ece1f
premji0133 <30617577+premji0133@users.noreply.github.com>
2017-08-06 15:41:55 +0530

    Update public.ts

--- server/routes/public.ts
+++ server/routes/public.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import { Tas_users } from '../models/tas_users';
+import { Tas_marks } from '../models/tas-marks';
 import { Sequelize, sequelize } from './dbcon';
 const publicRouter: Router = Router();
 
@@ -38,6 +39,33 @@ then(users => {
      });
  });
    
+});
+
+
+ publicRouter.get('/create_marks_table', (request: Request, response: Response) => {
+ Tas_marks.sync({force: true}).then(() => {
+  //Table created
+  return Tas_users.create({
+    NAME: 'Prems021',
+    TESTNO: 11,
+    MARK: 100,
+    PERCENTAGE: 100
+     });
+ });
+   
+ });
+
+
+ publicRouter.post('/submit_exam', (request: Request, response: Response) => {
+ Tas_users.create({
+    NAME: request.body.username,
+    TESTNO: request.body.testno,
+    MARK: request.body.mark,
+    PERCENTAGE: request.body.percentage
+     })
+     
+  return response.json({success:true, msg:'Successfully saved'});
+     
  });
 
 
@@ -48,9 +76,10 @@ then(users => {
     MARK: 10,
     PASSWORD: request.body.password
      })
-   return response.json({success:true, msg:'Successfully saved'});
+     
+  return response.json({success:true, msg:'Successfully saved'});
+     
  });
-   
 
   publicRouter.post('/login_check', (request: Request, response: Response) => {
    var username = request.body.username;
@@ -58,8 +87,8 @@ then(users => {
     
     Tas_users.findOne({
   where: {
-    NAME: username,
-    PASSWORD: password
+    NAME: request.body.username,
+    PASSWORD: request.body.password
     }
 }).then(function(result){
   
@@ -74,9 +103,14 @@ then(users => {
                                                 response.status(403).send({success: false, msg: 'Authentication failed, User not found'});
                                                 }    
                          });
-         
+      
+   
+ 
+
+   
  });
-  
+
+ 
  
 
 
